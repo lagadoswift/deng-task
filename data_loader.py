c10bcd9aabc7e867ae1dd82c89ed06bff3fe5a31
Denis Kirillov <kirillovdm2002@gmail.com>
2025-09-29 17:58:35 +0300

    Task 3 update

--- data_loader.py
+++ data_loader.py
@@ -1,22 +1,30 @@
-#Data loader (task №2)
+# Data loader (task №2)
 
 import pandas as pd
+import os
 
-url='https://drive.google.com/uc?id=1dTauyYXM4FfCyeFgc6jAS9XtkjTdfsVt'
+url = "https://drive.google.com/uc?id=1dTauyYXM4FfCyeFgc6jAS9XtkjTdfsVt"
 
-df=pd.read_csv(url)
+df = pd.read_csv(url)
 
 print(df.head())
 
-#saving the dataset (for offline access)
-
-import os
+# saving the dataset (for offline access)
 
 script_dir = str(os.path.dirname(__file__))
-print('SCRIPT DIRECTORY:',script_dir)
-files_dir = script_dir + '\\data\\'
+print("SCRIPT DIRECTORY:", script_dir)
+files_dir = script_dir + "\\data\\"
 
 if not os.path.exists(files_dir):
     os.makedirs(files_dir)
 
-df.to_csv(files_dir+'dataset.csv',index=False)
+df.to_csv(files_dir + "dataset.csv", index=False)
+
+# Task №3
+
+print(df.describe())
+print(df.dtypes)
+
+# saving to Parquet
+
+df.to_parquet(files_dir + "dataset.parquet", index=False)
